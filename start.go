a3d998b2c77f9f95675cac2b64abed4531b1aaf3
ApolloTitan <mhagspiel@gmail.com>
2017-04-10 22:04:24 +0200

    update

---
+++ start.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	first string
+	last string
+	age int
+}
+
+func main() {
+	p1 := Person{"james", "bond", 20}
+	p2 := Person{"miss", "moneypenny", 18}
+	fmt.Println(p1.first, p1.last, p1.age)
+	fmt.Println(p2.first, p2.last, p2.age)
+}
