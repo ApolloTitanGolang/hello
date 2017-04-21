7ca4a6fe930ad94286dca489e03cef6775441678
ApolloTitan <mhagspiel@gmail.com>
2017-04-21 14:49:42 +0200

    update

--- binarySearchTree.go
+++ binarySearchTree.go
@@ -3,83 +3,104 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"encoding/json"
+	"net/http"
+	"time"
 )
 
 type Tree struct {
-	root *Node	`json:"Node"`
-	count int	`json:"Count"`
-	name string	`json:"Name"`
+	Root  *Node	`json:"node"`
+	Count int	`json:"count"`
+	Name  string	`json:"name"`
 }
 
 func (t *Tree) addNode(n Node) {
-	if t.root == nil {
-		t.root = &n
+	if t.Root == nil {
+		t.Root = &n
 	} else {
-		t.root.addChildNode(n)
+		t.Root.addChildNode(n)
 	}
-	t.count++
+	t.Count++
 }
 
 type Node struct {
-	value int
-	leftChild *Node
-	rightChild *Node
+	Value int		`json:"value"`
+	Count int		`json:"count"`
+	LeftChild *Node		`json:"leftChild"`
+	RightChild *Node	`json:"rightChild"`
 }
 
 func (parent *Node) addChildNode(newNode Node) {
 	switch {
-	case (newNode.value < parent.value) && (parent.leftChild == nil):
-		parent.leftChild = &newNode
-	case (newNode.value > parent.value) && (parent.rightChild == nil):
-		parent.rightChild = &newNode
-	case (newNode.value < parent.value) && (parent.leftChild != nil):
-		parent.leftChild.addChildNode(newNode)
-	case (newNode.value > parent.value) && (parent.rightChild != nil):
-		parent.rightChild.addChildNode(newNode)
+	case (newNode.Value < parent.Value) && (parent.LeftChild == nil):
+		parent.LeftChild = &newNode
+	case (newNode.Value > parent.Value) && (parent.RightChild == nil):
+		parent.RightChild = &newNode
+	case (newNode.Value < parent.Value) && (parent.LeftChild != nil):
+		parent.LeftChild.addChildNode(newNode)
+	case (newNode.Value > parent.Value) && (parent.RightChild != nil):
+		parent.RightChild.addChildNode(newNode)
 	}
 }
 
 func (start *Node) visit() {
-	if start.leftChild != nil {
-		start.leftChild.visit()
+	if start.LeftChild != nil {
+		start.LeftChild.visit()
 	}
-	fmt.Printf("\n - %v", start.value)
-	if start.rightChild != nil {
-		start.rightChild.visit()
+	fmt.Printf("\n - %v - %v", start.Value, start.Count)
+	if start.RightChild != nil {
+		start.RightChild.visit()
 	}
 }
 
 func (start *Node) search(value int) {
-	if start.value == value {
-		fmt.Printf("\nfound: %v", start.value)
-	} else if (value < start.value) && (start.leftChild != nil) {
-		start.leftChild.search(value)
-	} else if (value > start.value) && (start.rightChild != nil) {
-		start.rightChild.search(value)
+	if start.Value == value {
+		fmt.Printf("\nfound: %v", start.Value)
+	} else if (value < start.Value) && (start.LeftChild != nil) {
+		start.LeftChild.search(value)
+	} else if (value > start.Value) && (start.RightChild != nil) {
+		start.RightChild.search(value)
+	}
+}
+
+func getJSON(tree *Tree) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m, _ := json.Marshal(tree)
+		fmt.Printf("\nJSON: %s", string(m))
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Write([]byte(m))
 	}
 }
 
 func main() {
+
 	// tree
 	tree := new(Tree)
 	*tree = Tree{nil, 0, "<my first tree"}
-	(*tree).name = "<oh, it's my second tree>"
+	(*tree).Name = "<oh, it's my second tree>"
 
 	// add nodes
-	for i := 1; i <= 111; i++ {
+	for i := 1; i <= 20; i++ {
+		rand.Seed(time.Now().UTC().UnixNano())
 		randValue := rand.Intn(100)
-		tree.addNode(Node{randValue, nil, nil})
+		tree.addNode(Node{randValue, i,nil, nil})
 	}
 
 	// test output
 	fmt.Printf("\ntree: %v", *tree)
-	fmt.Printf("\ntree count: %v", tree.count)
-	fmt.Printf("\nroot: %v", tree.root.value)
-	fmt.Printf("\nfirst left: %v", tree.root.leftChild.value)
-	fmt.Printf("\nfirst right: %v", tree.root.rightChild.value)
-	fmt.Printf("\nsecond right: %v", tree.root.rightChild.rightChild.value)
+	fmt.Printf("\ntree count: %v", tree.Count)
+	fmt.Printf("\nroot: %v", tree.Root.Value)
+	fmt.Printf("\nfirst left: %v", tree.Root.LeftChild.Value)
+	fmt.Printf("\nfirst right: %v", tree.Root.RightChild.Value)
+	fmt.Printf("\nsecond right: %v", tree.Root.RightChild.RightChild.Value)
 
 	// ordered output
-	(*tree).root.visit()
-	(*tree).root.search(91)
+	(*tree).Root.visit()
+	(*tree).Root.search(91)
+
+	// start JSON Server
+	http.HandleFunc("/", getJSON(tree))
+	http.ListenAndServe(":8080", nil)
 }
